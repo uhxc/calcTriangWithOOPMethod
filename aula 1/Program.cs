7c26f7c52833a4120a400bf18ab67c5cfb3175a4
migue <migue@DESKTOP-5U5RGJB>
2023-06-21 16:16:11 -0300

    Add project files.

---
+++ aula 1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace aula_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("entre com os dados do triangulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+            
+            double xarea = x.Area();
+            Console.WriteLine($"A area de x é: {xarea:F4}");
+
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double yarea = y.Area();
+            Console.WriteLine($"A area de y é: {yarea:F4}");
+
+            if (xarea >= yarea) Console.WriteLine("o maior é x");
+            else Console.WriteLine("o maior é Y");
+
+        }
+    }
+}
---
+++ aula 1/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_1
+{
+    internal class Triangulo
+    {
+        public double A, B, C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2 ;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
